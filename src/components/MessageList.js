1bd91aa9a55ef0c1df136086f991b7b7022ec84d
kanno <kannoryo@icloud.com>
2020-12-02 10:46:08 +0900

    apply grid row

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import {makeStyles} from '@material-ui/core/styles'
+import { red } from '@material-ui/core/colors'
+
+const useStyles = makeStyles({
+    root: {
+        gridRow: 1,
+    }
+})
 
 const MessageList = () =>{
+    const classes=useStyles()
     return(
-        <div>MessageList</div>
+        <div className={classes.root}>MessageList</div>
     )
 }
 
