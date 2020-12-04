06f52bbda49512a3f0969f69bec8326d7870b99e
kanno <kannoryo@icloud.com>
2020-12-04 21:30:52 +0900

    display new message

--- src/components/MessageItem.js
+++ src/components/MessageItem.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React,{useRef, useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {ListItem, ListItemAvatar, Avatar, ListItemText, Typography} from '@material-ui/core'
 
 import {gravatarPath} from '../gravatar'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
     inline: {
       display: 'inline',
     },
 }));
 
-const MessageItem = ({name, text})=>{
+
+const MessageItem = ({isLastItem, name, text})=>{
+    const ref = useRef(null)
     const classes = useStyles()
     const avatarPath = gravatarPath(name)
-
-    console.log(name)
+    
+    useEffect(()=>{
+        if(isLastItem){
+            ref.current.scrollIntoView({ behavior: 'smooth'})
+        }
+    },[isLastItem])
     return(
-        <ListItem divider={true}> 
+        <ListItem divider={true} ref={ref}> 
         <ListItemAvatar>
           <Avatar src={avatarPath} />
         </ListItemAvatar>
